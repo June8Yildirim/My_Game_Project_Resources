242f2db725082635e1baba26ffba27c49adf928f
June8Yildirim <66858810+June8Yildirim@users.noreply.github.com>
2020-12-03 02:52:02 -0500

    I created basic my hangman game

--- Hangman/Hangman_mine.java
+++ Hangman/Hangman_mine.java
@@ -1,33 +1,55 @@
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Hangman_mine {
-    public static void main(String[]args){
-hangman();
+    public static void main(String[] args) {
+        hangman();
     }
-    public static void hangman(){
+
+    public static void hangman() {
         Scanner sc = new Scanner(System.in);
-//        int res = 0;
+// selecting word list
         String[] guessList = {"mental", "strengths", "were", "quite", "different", "as", "he", "revealed", "to", "his",
-                "colleague", "Jacques", "Hadamard"};
-//        Random rd = new Random();
-        int rdnumer = (int) (Math.random() * 11);
-        String word = guessList[rdnumer + 1];
-        char[] dash = new char[word.length()];
-        int n = word.length();
-        int f = 0; [7-0987654321
-
-
-
-
-    '']
-
-        for (int i=0;i<n;i++){
-            dash[i]='-';
-            if (word.charAt(i)==' '){
-                dash[i]=' ';
+                "colleague", "jacques", "hadamard"};
+        Random rd = new Random();
+        int rdnumer = rd.nextInt(guessList.length);
+        String selectedWord = guessList[rdnumer];
+        System.out.println(selectedWord);
+//        dashed list letter
+        char[] dash = new char[selectedWord.length()];
+        int length = selectedWord.length();
+// disappear letter with #
+        for (int i = 0; i < length; i++) {
+            dash[i] = '#';
+            if (selectedWord.charAt(i) == ' ') {
+                dash[i] = ' ';
             }
         }
-        System.out.println(dash);
+//        int i = 0;
+        System.out.println(Arrays.toString(dash));
+        while (length - 3 > 0) {
+            System.out.println("Type any letter");
+            char guessingLetter = sc.next().toLowerCase().charAt(0);
+            for (int i = 0; i < selectedWord.length(); i++) {
+                if (selectedWord.charAt(i) == guessingLetter) {
+                    dash[i] = guessingLetter;
+                }
+            }
+            System.out.println("Do you want to guess the word and finish the game. if yes type 'y");
+            char answer = sc.next().charAt(0);
+            if (answer == 'y') {
+                System.out.println("Type the word");
+                String answerWord = sc.next();
+                if (answerWord.equals(selectedWord)) {
+                    System.out.println("your answer is correct");
+                    length = 0;
+                } else {
+                    System.out.println("Your answer is wrong keep guess the letter.");
+                }
+            }
+            length--;
+            System.out.println(Arrays.toString(dash));
+        }
     }
-
 }
